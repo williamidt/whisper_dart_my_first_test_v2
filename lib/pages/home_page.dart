ef8a149fb1778cd8bd9aa5c5c7a2605edfb958a6
William P. Santos <williamidt>
2023-02-25 23:27:18 -0300

    atualização do package whisper_dart para 0.0.10 incluido também de forma simples getx

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,26 +1,23 @@
 import 'dart:io';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:whisper_dart/whisper_dart.dart';
 
+class HomePage extends StatelessWidget {
+  /// getx
+  RxString model = ''.obs;
+  RxString wav = ''.obs;
+  RxString transcribe = ''.obs;
+  RxBool isProcessing = false.obs;
+  RxString erro = ''.obs;
 
+  /// constructor
+  HomePage({Key? key}) : super(key: key);
 
-class HomePage extends StatefulWidget {
-  @override
-  _HomePageState createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-
-  String model = "";
-  String wav = "";
-  String transcribe = "";
-  bool isProcessing = false;
-
-
+  /// screen logic
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Center(
         child: Column(
@@ -33,58 +30,60 @@ class _HomePageState extends State<HomePage> {
                 fontWeight: FontWeight.bold,
               ),
             ),
-
-            Text("Path File Model: ${model}"),
-            Text("Path File Wav: ${wav}"),
-
+            Obx(() => Text("Path File Model: ${model.value}")),
+            Obx(() => Text("Path File Wav: ${wav.value}")),
             Row(
-              mainAxisAlignment:  MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 // button to set model
                 ElevatedButton(
-                onPressed: () async {
-                  FilePickerResult? resul =  await FilePicker.platform.pickFiles();
-                   if (resul != null) {
+                  onPressed: () async {
+                    FilePickerResult? resul =
+                        await FilePicker.platform.pickFiles();
+                    if (resul != null) {
                       File file = File(resul.files.single.path!);
                       if (file.existsSync()) {
-                      setState(() {
-                          model = file.path;
-                      });
+                        model.value = file.path;
+                        erro.value = "";
+                      }
                     }
-                  }
-                },
-
-                child: const Text("set model"),
+                  },
+                  child: const Text("set model"),
                 ),
 
                 const SizedBox(width: 10),
 
-
                 // button to set wav
                 ElevatedButton(
-                onPressed: () async {
+                  onPressed: () async {
                     FilePickerResult? resul =
-                    await FilePicker.platform.pickFiles();
+                        await FilePicker.platform.pickFiles();
                     if (resul != null) {
-                        File file = File(resul.files.single.path!);
-                    if (file.existsSync()) {
-                    setState(() {
-                      wav = file.path;
-                    });
+                      File file = File(resul.files.single.path!);
+                      if (file.existsSync()) {
+                        wav.value = file.path;
+                        erro.value = "";
+                      }
                     }
-                }
-                },
+                  },
                   child: const Text("set audio"),
                 ),
-                ],
+              ],
             ),
             const SizedBox(height: 30),
             ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  isProcessing = true;
-                  transcribe =  runningWhisperDart();
-                });
+              onPressed: () async {
+                if (model.value.trim().isNotEmpty &&
+                    wav.value.trim().isNotEmpty) {
+                  await runningWhisperDart();
+                } else {
+                  if (wav.value.trim().isEmpty) {
+                    erro.value = "Please, select wav file before to the running";
+                  }
+                  if (model.value.trim().isEmpty) {
+                   erro.value = "Please, select the model *.bin";
+                  }
+                }
               },
               child: const Text(
                 "To start Transcribe Audio...",
@@ -95,10 +94,17 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
             const SizedBox(height: 30),
+            Obx(() => isProcessing.isTrue ?
+                const CircularProgressIndicator(): const Center()),
 
-            isProcessing == false
-            ? Flexible(child: Text("Result to Transcribe: ${transcribe}"))
-                : const CircularProgressIndicator(),
+            Obx(() => Text("Result to Transcribe: ${transcribe.value}")),
+
+            const SizedBox(height: 30),
+
+            Obx(() => Text(erro.value,
+                style: const TextStyle(
+                  color: Colors.red,
+                ))),
           ],
         ),
       ),
@@ -107,37 +113,32 @@ class _HomePageState extends State<HomePage> {
 
   /// function to whisper dart in simple mode
   runningWhisperDart() async {
+    isProcessing.value = true;
 
+    /// process lib whisper
     Whisper whisper = Whisper();
-
-    var get_version =
-    whisper.request(whisperRequest: WhisperRequest({"@type": "getVersion"}));
+    var get_version = whisper.request(
+        whisperRequest: WhisperRequest({"@type": "getVersion"}));
     print(get_version);
     try {
-      var res = whisper.request(
+      var res = await whisper.request(
         whisperRequest: WhisperRequest.fromWavFile(
-          audio: File(wav),
-          model: File(model),
+          audio: File(wav.value),
+          model: File(model.value),
           language: "en",
+
+          /// configured english transcribe en , if you need check your code in google page below
+          /// https://cloud.google.com/translate/docs/languages
         ),
       );
       print(res.toString());
-      transcribe =  res.toString();
+      isProcessing.value = false;
+      transcribe.value = res.toString();
+      erro.value = '';
     } catch (e) {
       print(e);
     }
-
-    setState(() {
-      isProcessing = false;
-    });
-
-
+    isProcessing.value = false;
   }
-
-
 }
-
-
-
-
 
